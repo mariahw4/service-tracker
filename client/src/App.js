f0e2bb774abc9f2ad9cd31dec95e96ff4f47dddf
Mariah Wear <wearmariahk@gmail.com>
2023-06-21 15:51:56 -0600

    more initial file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import {
+//     ApolloClient,
+//     ApolloProvider,
+//     InMemoryCache,
+//     createHttpLink,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+
+
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navigation from './components/navbar';
+import Dashboard from "./pages/dashboard";
+import Homepage from "./pages/homepage";
+
+// const httpLink = createHttpLink({
+//   uri: "/graphql",
+// });
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem("id_token");
+//   // return the headers to the context so httpLink can read them
+//   return {
+//       headers: {
+//           ...headers,
+//           authorization: token ? `Bearer ${token}` : "",
+//       },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
+//     <ApolloProvider client={client}>
+//     <Router>
+//         <>
+//             <Navigation />
+//             <Switch>
+//                 <Route path="/" element={< Homepage />} />
+//                 <Route path="/dashboard" element={ <Dashboard />} />
+//                 <Route
+//                     render={() => (
+//                         <h1 className="display-2">Wrong page!</h1>
+//                     )}
+//                 />
+//             </Switch>
+//         </>
+//     </Router>
+// </ApolloProvider>
+// );
+
+
+
     <div className="App">
       < Navigation />
-      <header className="App-header">
+      < Homepage />
+      < Dashboard />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to my Service Tracker App!!
@@ -20,7 +78,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+    return (
+        <>
+        <h1>Welcome to the dashboard</h1>
+        </>
+    )
+}
+
+export default Dashboard;
